04c6ba489dd77407dd64ca433e6765a716c8c875
Elvinardy <elvin.ardy@gmail.com>
2021-12-14 15:16:57 +0800

    4th commit

--- assessment/src/main/java/sdf/HttpClientConnection.java
+++ assessment/src/main/java/sdf/HttpClientConnection.java
@@ -1,17 +1,87 @@
 package sdf;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class HttpClientConnection extends Thread {
+public class HttpClientConnection {
 
-System.out.println("Creating client socket.. ");
-Socket s = new Socket("localhost",3066); 
-    Scanner userInput = new Scanner(System.in);
+    private final Socket s;
+    private HttpWriter writer;
+    private BufferedReader br;
+    String line = "";
+
+public HttpClientConnection (Socket s) {   //Constructor
+    this.s = s;
+
+}
+
+public void run() {
+  
+}
+private void startStream() {
+
+try {
+    writer = new HttpWriter(s.getOutputStream());
+    br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    line = br.readLine();
+    
+} catch (IOException ioe) {
+    System.out.println("Something went wrong..");
+}
 
 
+
+private String[] getFromBrowser() throws IOException {
+    String line = br.readLine();
+    String[] argsRequest = line.split(" ");
+    return argsRequest;
+
+}
+// separate the request
+public void checkRequest(String[] argsRequest) {
+    String method = argsRequest[0];
+    String link = argsRequest[1];
+
+    if(!method.equals("GET")) {
+        this.writer.writeString("405 Method not Allowed");
+        return;
+    }
+    if(!Paths.get(link).toFile().exists()) {
+        this.writer.writeString("404 not found..\n");
+        this.writer.writeString(link + "not found");
+        return;
+    } else {
+        String fileDir = file.Opt.get();
+        if (fileHandler.isPNG(link)) {
+            this.writePNG(filDir);
+            return;
+        }
+        this.write(fileDir);
+    }
+
+}
+// check if GET
+// if not get, write 405
+
+// get if file exists 
 public void checkPath (String file) {
     
 }
+// if not write 404
+
+
+//check if file is png
+
+// return 200 OK
+// return page or png
+
+
 
 
 
